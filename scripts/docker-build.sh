6aafa450c19ae225c8a2735909d1c18e9a48d687
Wesley Charles Blake <wesley@po.et>
2018-08-29 15:37:48 +0200

    chore[travis]: fix semver-docker

--- scripts/docker-build.sh
+++ scripts/docker-build.sh
@@ -4,7 +4,3 @@ TAG="v-$(git log -1 --pretty=%h)-beta";
 
 docker build -t "${REPO}":"${TAG}" .
 
-if [ "${TRAVIS_BRANCH}" == "master" ] && [ "${TRAVIS_PULL_REQUEST_BRANCH}" = "" ]; then 
-    docker tag "${REPO}":"${TAG}" "${REPO}":latest;
-fi
-
