931dea6fc609d5393585d25f8afd58e899ca717b
Wesley Charles Blake <wesley@po.et>
2018-08-29 12:55:08 +0200

    more tests

--- scripts/docker-push.sh
+++ scripts/docker-push.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
 
-docker login -u "$DOCKER_USER" -p "$DOCKER_PASS"
+docker login -u "${DOCKER_USER}" -p "${DOCKER_PASS}"
 
-docker push "$REPO"
+docker push "${REPO}"
--- scripts/docker-tag.sh
+++ scripts/docker-tag.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-if [ -n "$TRAVIS_TAG" ]; then 
-    TAG=$TRAVIS_TAG; else
+if [ -n "${TRAVIS_TAG}" ]; then 
+    TAG=${TRAVIS_TAG}; else
     TAG=v-$(git log -1 --pretty=%h)-beta;
 fi
 
-if [ "$TRAVIS_BRANCH" == "master" ] && [ "$TRAVIS_PULL_REQUEST_BRANCH" = "" ]; then 
-    docker tag "$REPO":"${TAG}" "$REPO":latest;  else
-    docker tag "$REPO":"${TAG}" ;
+if [ "${TRAVIS_BRANCH}" == "master" ] && [ "${TRAVIS_PULL_REQUEST_BRANCH}" = "" ]; then 
+    docker tag "${REPO}":"${TAG}" "${REPO}":latest;  else
+    docker tag "${REPO}":"${TAG}" ;
 fi
