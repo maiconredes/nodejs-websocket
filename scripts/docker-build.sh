c9290d9bbd1f152df2d1b1ae9ad84a841106931a
Wesley Charles Blake <wesley@po.et>
2018-08-29 16:53:57 +0200

    fix(travis): semver docker tag

--- scripts/docker-build.sh
+++ scripts/docker-build.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
 
-TAG="v-$(git log -1 --pretty=%h)-beta";
-
+# always tag on git hash
+TAG=v-$(git log -1 --pretty=%h)-beta;
 docker build -t "${REPO}":"${TAG}" .
 
+# if git tag release, tag semver
+if [ -n "${TRAVIS_TAG}" ]; then 
+    docker tag "${REPO}":"${TAG}" "${REPO}":"${TRAVIS_TAG}"
+fi
+
+# if master is updated, update the latest tag
+if [ "$TRAVIS_BRANCH" == "master" ]; then 
+    docker tag "${REPO}":"${TAG}" "${REPO}":latest;
+fi
