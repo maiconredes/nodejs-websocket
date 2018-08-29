b02b1aa082441e79fa751d6fae539e9db6f2dd40
Wesley Charles Blake <wesley@po.et>
2018-08-29 13:32:44 +0200

    [feat]: semver release

--- scripts/docker-build.sh
+++ scripts/docker-build.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
 
-docker build -t "${REPO}" .
+if [ -n "${TRAVIS_TAG}" ]; then 
+    TAG=${TRAVIS_TAG}; else
+    TAG="v-$(git log -1 --pretty=%h)-beta";
+fi
+
+docker build -t "${REPO}":"${TAG}" .
+
+if [ "${TRAVIS_BRANCH}" == "master" ] && [ "${TRAVIS_PULL_REQUEST_BRANCH}" = "" ]; then 
+    docker tag "${REPO}":"${TAG}" "${REPO}":latest;
+fi
+
