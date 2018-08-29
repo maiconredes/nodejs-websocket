9fd7078860c0edb2cdc132c57c38db0acfa93d19
Wesley Charles Blake <wesley@nikordi.com>
2018-08-29 16:00:41 +0200

    Docker smver (#7)
    * chore(travis and docker): try the docker semver plugin
    
    * chore(travis): fix config
    
    * fix(ci): sort out semver
    
    * chore[travis]: fix semver-docker
    
    * chore(travis): Docker smver (#5) (#6)
    
    * chore(travis and docker): try the docker semver plugin
    
    * chore(travis): fix config

--- scripts/docker-build.sh
+++ scripts/docker-build.sh
@@ -1,13 +1,6 @@
 #!/bin/bash
 
-if [ -n "${TRAVIS_TAG}" ]; then 
-    TAG=${TRAVIS_TAG}; else
-    TAG="v-$(git log -1 --pretty=%h)-beta";
-fi
+TAG="v-$(git log -1 --pretty=%h)-beta";
 
 docker build -t "${REPO}":"${TAG}" .
-
-if [ "${TRAVIS_BRANCH}" == "master" ] && [ "${TRAVIS_PULL_REQUEST_BRANCH}" = "" ]; then 
-    docker tag "${REPO}":"${TAG}" "${REPO}":latest;
-fi
 
