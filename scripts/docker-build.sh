74caa8619bfe2c87bd1740494f2085eb998e4749
Wesley Charles Blake <wesley@po.et>
2018-08-29 12:58:53 +0200

    fix order of tagging

--- scripts/docker-build.sh
+++ scripts/docker-build.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker build -t "${REPO}":"${TAG}" .
+docker build -t "${REPO}" .
--- scripts/docker-tag.sh
+++ scripts/docker-tag.sh
@@ -2,7 +2,7 @@
 
 if [ -n "${TRAVIS_TAG}" ]; then 
     TAG=${TRAVIS_TAG}; else
-    TAG=v-$(git log -1 --pretty=%h)-beta;
+    TAG="v-$(git log -1 --pretty=%h)-beta";
 fi
 
 if [ "${TRAVIS_BRANCH}" == "master" ] && [ "${TRAVIS_PULL_REQUEST_BRANCH}" = "" ]; then 
